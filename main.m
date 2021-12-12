7ffae54c61d388800d266587a9750e6e8b0056df
Benjamin <ge93leh@tum.de>
2021-12-12 22:15:56 +0100

    implemented possible infection if agent is in contact with infected agent

--- main.m
+++ main.m
@@ -20,13 +20,15 @@ while infections <= n_infected_agents
     end
 end
 
-
-
-%% Moving agents
+risk_of_infection = 0.2; %risk of infection if in contact with infected person
+infection_radius = 1;
+%% Simulation
 timestep = 0.1;
 vel_scaling = 2;
 steps = 100;
-for j = 1:steps
+step = 0;
+while step <= steps
+    %Moving
     for i = 1:n_agents
         direction = randi(360);
         vel = rand() * vel_scaling;
@@ -38,7 +40,23 @@ for j = 1:steps
         end
         agents(i).position = move;
     end
+
+    %Infection?
+    for i = 1:n_agents %Iterating over every agent
+        if agents(i).infected == 0 %only if not infected yet
+            for j = 1:n_agents %Iterating over every agent except the active one (j~=i)
+                if j ~=i
+                    if abs(agents(i).position - agents(j).position) < infection_radius %Risk of infection
+                        if (rand() < risk_of_infection)
+                            agents(i).infected = 1;
+                        end
+                    end
+                end
+            end
+        end
+    end
     
+    %Plot
     hold on
     for i = 1:n_agents
         if agents(i).infected == 1
@@ -50,12 +68,8 @@ for j = 1:steps
     xlim([-0.3,10.3])
     ylim([-0.3,10.3])
     hold off
+
+    step = step + 1;
+    pause(0.1);
 end
-%% Plotting curves
-% hold on
-% for i = 1:n
-%     plot(agents(i).position(1), agents(i).position(2),'o')
-% end
-% xlim([0.7,10.3])
-% ylim([0.7,10.3])
-% hold off
+
