180b51059a07e22f3138b74e075efd6c009e952c
Benjamin <ge93leh@tum.de>
2021-12-26 20:28:22 +0100

    cleanup

--- main.m
+++ main.m
@@ -1,7 +1,14 @@
 clear; clc; close all;
+%% Parameters
+n_agents = 100;
+starting_infection_rate = 0.01;  
+risk_of_infection = 0.1;
+infection_radius = 0.3;
+quarantine_prob = 0.7;
+live_visualisation = 0;
+max_xy = 10;
 
 %% Initialising agents with random positions in max_xy square
-n_agents = 40;
 max_xy = 10;
 agents = agent.empty(0,0);
 for i = n_agents:-1:1
@@ -9,9 +16,8 @@ for i = n_agents:-1:1
 end
 
 %% Infection
-starting_infection_rate = 0.1; %starting infection rate
 starting_n_infected_agents = starting_infection_rate * n_agents;
-infections = 1;
+infections = 0;
 while infections <= starting_n_infected_agents
     index = randi([1,n_agents]);
     if agents(index).infected == 0 %not infected yet
@@ -20,12 +26,16 @@ while infections <= starting_n_infected_agents
     end
 end
 
-risk_of_infection = 0.2; %risk of infection if in contact with infected person
-infection_radius = 0.3;
 
 %% Quarantine
-quarantine_prob = 1; %probability that infections get's detected and agent is put in quarantine
 quarantine_pos = [100;100]; %position for quarantine area
+
+%% Initialise visualisation figure
+if live_visualisation == 1
+    figure(1)
+    xlim([-0.3, max_xy + 0.3])
+    ylim([-0.3, max_xy + 0.3])
+end
 
 %% Simulation
 timestep = 0.1;
@@ -34,6 +44,7 @@ steps = 100;
 step = 0;
 n_infected_agents = starting_n_infected_agents;
 infection_data = [];
+
 while step <= steps
     %Moving
     for i = 1:n_agents
@@ -59,6 +70,10 @@ while step <= steps
                         if (rand() < risk_of_infection)     %infection?
                             agents(i).infected = 1;
                             n_infected_agents = n_infected_agents + 1;
+                            if (rand() < quarantine_prob)
+                                agents(i).quarantine = 1;
+                                agents(i).position = quarantine_pos;
+                            end
                         end
                     end
                 end
@@ -72,30 +87,38 @@ while step <= steps
         end
     end
     
-%     %Plot of agents
-%     figure(1)
-%     hold on
-%     for i = 1:n_agents
-%         if agents(i).infected == 1
-%             plot(agents(i).position(1), agents(i).position(2),'ro')
-%         else
-%             plot(agents(i).position(1), agents(i).position(2),'ko')
-%         end
-%     end
-%     xlim([-0.3,max_xy + 0.3])
-%     ylim([-0.3,max_xy + 0.3])
-%     hold off
+    %Plot of agents
+    if live_visualisation == 1
+        figure(1)
+        hold on
+        for i = 1:n_agents
+            if agents(i).infected == 1
+                plot(agents(i).position(1), agents(i).position(2),'ro')
+            else
+                plot(agents(i).position(1), agents(i).position(2),'ko')
+            end
+        end
+        hold off
+    end
 
     %Plot for analytics
-    figure(2)
     infection_data = [infection_data; n_infected_agents n_agents-n_infected_agents];
-    bar(infection_data,'stacked');
-    legend('infected','healthy')
-    
+    if live_visualisation == 1
+        figure(2)
+        bar(infection_data,'stacked');
+        legend('infected','healthy')
+    end
+
 
     %Increase simulation step
     step = step + 1;
     %Pause
     pause(0.1);
+end
+
+if live_visualisation ~= 1
+    figure(2) 
+    bar(infection_data,'stacked');
+    legend('infected','healthy');
 end
 
