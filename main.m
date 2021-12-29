23101dd6083c97f8e5a5fdd73f2e83bed0137cad
Benjamin <ge93leh@tum.de>
2021-12-29 22:13:10 +0100

    live visualisation with clearing figure every three steps

--- main.m
+++ main.m
@@ -1,11 +1,11 @@
 clear; clc; close all;
 %% Parameters
-n_agents = 10;
+n_agents = 100;
 starting_infection_rate = 0.01;  
 risk_of_infection = 0.1;
 infection_radius = 0.7;
 quarantine_prob = 0.5;
-live_visualisation = 0;
+live_visualisation = 1;
 max_xy = 10;
 
 %% Initialising agents with random positions in max_xy square
@@ -80,10 +80,69 @@ while step <= steps
 end
 
 if live_visualisation ~= 1
-    figure(2) 
+    figure(1) 
     bar(infection_data,'stacked');
     legend('infected','healthy');
 end
+
+%% Visualisation of precomputed data
+step = 1;
+clear_intervall = 3;
+iter_clear = 1;
+pos1 = [0.05 0.2 0.4 0.6];
+pos2 = [0.6 0.2 0.4 0.6];
+
+if live_visualisation == 1
+    figure1 = subplot('Position',pos1);
+    figure2 = subplot('Position',pos2);
+    figure1;
+    xlim([-0.3, max_xy + 0.3]);
+    ylim([-0.3, max_xy + 0.3]);
+
+    %Pause
+    pause(2) %to open up window
+
+    while step <= steps
+        
+        %Plot of persons
+        subplot('Position',pos1)
+        hold on
+        for i = 1:n_agents
+            if agents(i).old_infection_status(step) == 1
+                if agents(i).old_quarantine_status(step) == 1
+                    plot(agents(i).old_positions(1,step), agents(i).old_positions(2,step), 'go') %infected person in qurantine --> green
+                else
+                    plot(agents(i).old_positions(1,step), agents(i).old_positions(2,step), 'ro') %infected person not in quarantine --> red
+                end
+            else
+                plot(agents(i).old_positions(1,step), agents(i).old_positions(2,step), 'ko') %not infected person --> black
+            end
+        end
+        hold off
+
+        %Plot of analytics
+        subplot('Position',pos2)
+        bar(infection_data(1:step,:),'stacked');
+        legend('infected','healthy');
+
+        %Pause
+        pause(0.01)
+
+        %increasing step
+        step = step + 1;
+        iter_clear = iter_clear + 1;
+        if iter_clear == clear_intervall
+            subplot('Position', pos1)
+            clf reset
+            subplot('Position',pos1)
+            xlim([-0.3, max_xy + 0.3]);
+            ylim([-0.3, max_xy + 0.3]);
+            iter_clear = 0;
+        end
+    end
+end
+
+
 
 
 
